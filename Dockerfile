b5df663200df1bee89b01aff56414185a67e1bfd
Ricardo Job <sousajob@gmail.com>
2016-09-23 10:24:45 -0300

    teste com docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,26 @@
-FROM jboss/wildfly
-ENV DEPLOY /opt/jboss/wildfly/standalone/deployments/
+FROM payara/server-full
+ENV DOMAIN domain1
+RUN echo 'AS_ADMIN_PASSWORD=admin\n\
+ EOF\n'\
+>> /opt/pwdfile
+
+RUN \
+ $PAYARA_PATH/bin/asadmin start-domain ${DOMAIN} && \
+ #$PAYARA_PATH/bin/asadmin --user admin --passwordfile=/opt/pwdfile enable-secure-admin && \
+ $PAYARA_PATH/bin/asadmin --user admin --passwordfile=/opt/pwdfile 
+
+
+ENV DEPLOY ${PAYARA_PATH}/glassfish/domains/${DOMAIN}/autodeploy/
+ENTRYPOINT $PAYARA_PATH/bin/asadmin start-domain --verbose ${DOMAIN}
+WORKDIR /opt/payara41/glassfish/bin
 ADD target/singleton.war ${DEPLOY}
-CMD ["/opt/jboss/wildfly/bin/standalone.sh", "-c", "standalone.xml", "-b", "0.0.0.0"]
+
+
+#FROM jboss/wildfly
+#ENV DEPLOY /opt/jboss/wildfly/standalone/deployments/
+#ADD target/singleton.war ${DEPLOY}
+#CMD ["/opt/jboss/wildfly/bin/standalone.sh", "-c", "standalone.xml", "-b", "0.0.0.0"]
+#RUN /opt/jboss/wildfly/bin/add-user.sh admin Admin#007 --silent
+#CMD ["/opt/jboss/wildfly/bin/standalone.sh", "-b", "0.0.0.0", "-bmanagement", "0.0.0.0"]
+#RUN /opt/jboss/wildfly/bin/add-user.sh -a ricardojob job.2016 --silent
+#CMD ["/opt/jboss/wildfly/bin/standalone.sh", "-c", "standalone.xml", "-b", "0.0.0.0"]
--- src/main/java/docker/ServiceLocatorNoDocker.java
+++ src/main/java/docker/ServiceLocatorNoDocker.java
@@ -20,8 +20,8 @@ public class ServiceLocatorNoDocker {
             Properties props = new Properties();
             props.put(Context.INITIAL_CONTEXT_FACTORY,
                     "com.sun.enterprise.naming.SerialInitContextFactory");
-            props.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
-//            props.setProperty("org.omg.CORBA.ORBInitialHost", "192.168.99.100");
+//            props.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
+            props.setProperty("org.omg.CORBA.ORBInitialHost", "192.168.99.100");
             props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
 //            props.setProperty("java.naming.factory.url.pkgs",
 //                    "com.sun.enterprise.naming");
@@ -32,17 +32,18 @@ public class ServiceLocatorNoDocker {
 //            props.put(Context.SECURITY_PROTOCOL, "ssl");
 
 //            props.put(Context.PROVIDER_URL, "iiop://192.168.99.100:3700");
+//prop.put(Context.PROVIDER_URL, "http-remoting://127.0.0.1:8080");
             props.put(Context.SECURITY_AUTHENTICATION, "simple");
             props.put(Context.SECURITY_PRINCIPAL, "admin");
             props.put(Context.SECURITY_CREDENTIALS, "admin");
 
             InitialContext context = new InitialContext(props);
-            
+
             NamingEnumeration<NameClassPair> list2 = context.list(context.getNameInNamespace());
             while (list2.hasMore()) {
                 System.out.println(list2.next());
             }
-           
+
             return (T) context.lookup(recurso);
 
         } catch (NamingException ne) {
@@ -53,4 +54,3 @@ public class ServiceLocatorNoDocker {
     }
 
 }
-
